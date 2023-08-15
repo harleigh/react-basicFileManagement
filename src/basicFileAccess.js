cfb749207ff7329a7bb74fc76c7fb1b9b21e013d
harleigh <harleigh.marsh@gmail.com>
2023-08-15 08:21:30 -0700

    initial commit

---
+++ src/basicFileAccess.js
@@ -0,0 +1,17 @@
+//every begining has a begining, and this is this begining. And, being that
+//this is the begining, take it with a grain of salt as there is quite a road
+//yet being paved; as I am learning, all code and documentation are "as is"
+
+
+/**
+ * 
+ */
+function FileAccess() {
+    return(
+        <>
+            <h1>Hello File!</h1>
+        </>
+    );
+}
+
+export default FileAccess
